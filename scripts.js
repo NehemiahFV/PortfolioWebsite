740608a3898ee9031038cc4bc1241e72d39b18b7
NehemiahFV <Nehemiah.FV@outlook.com>
2022-07-16 21:36:28 +0100

    update files

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 
 
-function moveToAboutMe(){
-    document.getElementById('about-section').scrollIntoView();
+// function moveToAboutMe(){
+//     document.getElementById('about-section').scrollIntoView();
+// }
+
+function moveToAboutMe(id){
+    //alert(id.id)
+    document.getElementById(id.id).scrollIntoView({
+        behavior: 'smooth'
+    });
 }
+
 
 
